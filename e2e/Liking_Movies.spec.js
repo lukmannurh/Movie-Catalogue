b98cf8000891d85f6054adf8363c986d62be6f49
unknown <luqmannur33@gmail.com>
2022-10-19 11:43:09 +0700

    Initialize movie catalogue automated testing implemented

---
+++ e2e/Liking_Movies.spec.js
@@ -0,0 +1,67 @@
+const assert = require('assert');
+
+Feature('Liking Movies');
+
+Before(({ I }) => {
+  I.amOnPage('/#/like');
+});
+
+Scenario('showing empty liked movies', ({ I }) => {
+  I.seeElement('#query');
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+});
+
+Scenario('liking one movie', async ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+  I.amOnPage('/');
+
+  I.waitForElement('.movie__title a', 10);
+
+  const firstFilm = locate('.movie__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
+
+  I.waitForElement('#likeButton', 10);
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.movie-item');
+  const likedFilmTitle = await I.grabTextFrom('.movie__title');
+
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
+});
+
+Scenario('Searching movies', async ({ I }) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.waitForElement('.movie__title a', 10);
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.movie__title a').at(i));
+    I.waitForElement('#likeButton', 10);
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.movie__title'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/like');
+  I.seeElement('#query');
+
+  const searchQuery = titles[1].substring(1, 3);
+  const matchingMovies = titles.filter((title) => title.indexOf(searchQuery) !== -1);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleLikedMovies = await I.grabNumberOfVisibleElements('.movie-item');
+  assert.strictEqual(matchingMovies.length, visibleLikedMovies);
+
+  matchingMovies.forEach(async (title, index) => {
+    const visibleTitle = await I.grabTextFrom(locate('.movie__title').at(index + 1));
+    assert.strictEqual(title, visibleTitle);
+  });
+});
